58dc57dbf9bd449b3a355acc63cb7d16bf44a8f1
WishPotato <wish92jfn@hotmail.com>
2016-02-28 12:08:59 +0100

    Moved Project to Github

---
+++ 3rd Person Control/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+    private float xRotation;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+        xRotation += Input.GetAxis("Vertical");
+        xRotation = Mathf.Clamp(xRotation, 0.0f, 90.0f);
+	}
+}
---
+++ 3rd Person Control/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControl : MonoBehaviour {
+
+    private CameraControl cam;
+
+    private float speed = 5.45f;
+    private float crossLimit = 0.689f;
+    private float yRotation;
+
+    private Vector3 dir;
+    private Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+        // Movement
+        dir = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        UpdatedDirection(dir);
+        rb.transform.Translate(dir * speed * Time.deltaTime);
+        // Y Rotation for character and camera
+        yRotation += Input.GetAxis("Mouse X");
+        this.transform.rotation = Quaternion.Euler(0.0f, yRotation, 0.0f);
+	}
+
+    // Fixes increased speed, by having two vectors greater or smaller than 0.
+    void UpdatedDirection(Vector3 vec)
+    {
+        if (vec.x >= crossLimit && vec.z >= crossLimit)
+        {
+            vec.x = crossLimit;
+            vec.z = crossLimit;
+        }
+        else if (vec.x >= crossLimit && vec.z <= -crossLimit)
+        {
+            vec.x = crossLimit;
+            vec.z = -crossLimit;
+        }
+        else if (vec.x <= -crossLimit && vec.z >= crossLimit)
+        {
+            vec.x = -crossLimit;
+            vec.z = crossLimit;
+        }
+        else if (vec.x <= -crossLimit && vec.z <= -crossLimit)
+        {
+            vec.x = -crossLimit;
+            vec.z = -crossLimit;
+        }
+    }
+}
